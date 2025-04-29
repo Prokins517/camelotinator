ee03c2aeb252fead935389423d1438d4c8b7a430
Prokins517 <epicjman517@gmail.com>
2025-04-29 15:04:38 -0500

    Renamed a file and made some changes to venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jonathanperkins/Documents/Coding Projects/Camelotinator/.venv"
+set -gx VIRTUAL_ENV "/Users/jonathanperkins/Documents/GitHub/camelotinator/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
